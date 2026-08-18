656c6bd719a3deba7cc35e5182a4b11418dfb0b6
Juexis <77648644+Juexis@users.noreply.github.com>
2026-08-17 22:57:11 -0400

    Add: valid-palindrome-ii - submission-6

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-6.py
@@ -0,0 +1,34 @@
+class Solution:
+# helper function to check if sliced version would be a palindrome
+    def isPalindrome(self, s: str, left: int, right: int) -> bool:
+        # using two pointers to save space instead of slicing, i and j serve the same purpose as left and right
+        new_s: str = s[left:right]
+            
+        while left < right:
+            if s[left] != s[right]:
+                return False
+
+            left += 1
+            right -= 1
+        return True
+    
+    def validPalindrome(self, s: str) -> bool:
+        left: int = 0
+        right: int = len(s) - 1
+
+        while left < right:
+
+            # check mismatch
+            if s[left] != s[right]:
+                # use helper function to check if next letters in line allow it to be palindrome
+                if self.isPalindrome(s, left + 1, right) or self.isPalindrome(s, left, right - 1):
+                    return True
+                # if mismatch and next letters don't make it a palindrome, then it must be false
+                else:
+                    return False
+
+            left += 1
+            right -= 1
+        return True
+
+        
