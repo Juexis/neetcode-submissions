f88d9c07452575ef7b0f9695c35cedaac4730b34
Juexis <77648644+Juexis@users.noreply.github.com>
2026-08-17 22:49:37 -0400

    Add: valid-palindrome-ii - submission-3

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-3.py
@@ -0,0 +1,31 @@
+class Solution:
+    def isPalindrome(self, s: str, left: int, right: int) -> bool:
+        small_s: str = s[left:right]
+            
+        while left < right:
+            if s[left] != s[right]:
+                return False
+
+            left += 1
+            right -= 1
+        return True
+    
+    def validPalindrome(self, s: str) -> bool:
+        left: int = 0
+        right: int = len(s) - 1
+
+        while left < right:
+
+            if s[left] != s[right]:
+                # check if the next letter in line is equal, allowing the current letter to be "deleted"
+                if self.isPalindrome(s, left + 1, right) or self.isPalindrome(s, left, right - 1):
+                    return True
+                return False
+
+            left += 1
+            right -= 1
+        return True
+
+
+
+
