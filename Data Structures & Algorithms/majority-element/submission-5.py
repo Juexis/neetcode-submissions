310336b4415fd36fb82a0aaedf6f4f7140b5910a
Juexis <77648644+Juexis@users.noreply.github.com>
2026-08-21 18:55:44 -0400

    Add: majority-element - submission-5

---
+++ Data Structures & Algorithms/majority-element/submission-5.py
@@ -0,0 +1,18 @@
+class Solution: # boyer-moore algorithm, see neetcode solution
+    def majorityElement(self, nums: List[int]) -> int:
+        result: int = 0
+        count: int = 0
+
+        result = nums[0] # set to the first value
+        for n in nums: 
+            # increment count for appearances of result(majority), decrement for appearances of any number that is not result
+            if n == result:
+                count += 1
+            else:
+                count -= 1
+
+            # if count goes negative, then the current majority is replaced as it is no longer eligible
+            if count < 0: 
+                result = n
+        
+        return result
